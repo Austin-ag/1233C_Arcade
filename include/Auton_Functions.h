63e11ce3143f10c6d5e8b7fdb51b49793edeb371
Austin Grimes <tinman10.ag@gmail.com>
2020-10-31 09:11:35 -0600

    Create Auton_Functions.h

---
+++ include/Auton_Functions.h
@@ -0,0 +1,13 @@
+#include "vex.h"
+void MoveForward(float distance,  float speed);//Auton Function:Move forward
+void MoveBackward(float distance,  float speed);//Auton Function:Move backword
+void TurnRight(float distance,  float speed);//Auton Function:Turn Right
+void TurnLeft(float distance, float speed);//Auton Function:turn left
+void startIntake(float direction);//Auton Function:start intake
+void stopIntake(int direction);//Auton Function:stop intake
+void driveInches(float inches , float direction);//Drive forward or back with pid
+//if direction = 1 than backward else forward
+
+
+
+
