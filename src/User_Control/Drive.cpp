b9d4c60243242cb74cc22306b8e3518e347d5d1b
Austin Grimes <tinman10.ag@gmail.com>
2020-10-31 09:10:53 -0600

    Create Drive.cpp

---
+++ src/User_Control/Drive.cpp
@@ -0,0 +1,46 @@
+#include "Drive.h"
+//using namespace vex;
+
+void Drive()
+{
+
+
+    int deadband = 15;
+
+ 
+    // Get the velocity percentage of the left motor. (Axis3 + Axis1)
+    int leftMotorSpeed = Controller1.Axis3.position() + Controller1.Axis4.position();
+    // Get the velocity percentage of the right motor. (Axis3 - Axis1)
+    int rightMotorSpeed = Controller1.Axis3.position() - Controller1.Axis4.position();
+
+    // Set the speed of the left motor. If the value is less than the deadband,
+    // set it to zero.
+    if (abs(leftMotorSpeed) < deadband)
+    {
+      // Set the speed to zero.
+      Left_back.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+      Left_front.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+    } 
+    else 
+    {
+      // Set the speed to leftMotorSpeed
+      Left_back.spin(vex::directionType::fwd, leftMotorSpeed, vex::velocityUnits::pct);
+      Left_front.spin(vex::directionType::fwd, leftMotorSpeed, vex::velocityUnits::pct);
+    }
+
+    // Set the speed of the right motor. If the value is less than the deadband,
+    // set it to zero.
+    if (abs(rightMotorSpeed) < deadband) 
+    {
+      // Set the speed to zero
+      Right_back.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+      Right_front.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+    } 
+    else 
+    {
+      // Set the speed to rightMotorSpeed
+      Right_back.spin(vex::directionType::fwd, rightMotorSpeed, vex::velocityUnits::pct);
+      Right_front.spin(vex::directionType::fwd, rightMotorSpeed, vex::velocityUnits::pct);
+    }
+ 
+}
