79451919fee3712408abe11c271b00066b526430
Austin Grimes <tinman10.ag@gmail.com>
2020-10-31 09:10:50 -0600

    Create Flywheel.cpp

---
+++ src/User_Control/Flywheel.cpp
@@ -0,0 +1,26 @@
+#include "flywheel.h"
+
+
+void flywheel()
+{
+ if(Controller1.ButtonL2.pressing())
+      {
+       
+        Flywheel.spin(vex::directionType::rev, 70, vex::velocityUnits::pct);
+        
+         
+      }else if(Controller1.ButtonR2.pressing())
+      {
+        
+        Flywheel.spin(vex::directionType::fwd, 70, vex::velocityUnits::pct);
+        
+      }else if(Controller1.ButtonY.pressing())
+      {
+        Flywheel.spin(vex::directionType::rev, 0, vex::velocityUnits::pct);
+        Flywheel.setStopping(vex::brakeType::hold);
+         
+      }
+
+
+ 
+}
